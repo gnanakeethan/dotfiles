c80222c29e371bb640f2b8fea4e4b7c6d6f880a0
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2023-01-20 13:53:07 +0530

    Adding time calculation using golang library
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- nvim/lua/user/themes/onehalf.lua
+++ nvim/lua/user/themes/onehalf.lua
@@ -1,24 +1,11 @@
-local lustrous = require("lustrous")
+local latitude = 6.99
+local longitude = 79.99
+local sunrise = io.popen("timer " .. latitude .. " " .. longitude .. " sunrise"):read("*all")
+local sunset = io.popen("timer " .. latitude .. " " .. longitude .. " sunset"):read("*all")
+local now = os.time()
 
-local function switch_colorscheme(sunrise_time, sunset_time)
-  local current_time = os.time()
-  if current_time > sunrise_time and current_time < sunset_time then
-    vim.api.nvim_command("colorscheme dayfox")
-  else
-    print(sunrise_time)
-    vim.api.nvim_command("colorscheme nightfox")
-  end
+if now > tonumber(sunrise) and now < tonumber(sunset) then
+  vim.api.nvim_command("colorscheme dayfox")
+else
+  vim.api.nvim_command("colorscheme nightfox")
 end
-
-local function check_time()
-  local lat = 6.91 -- Example latitude
-  local lng = 79.99 -- Example longitude
-  local date = os.date("*t")
-  local zenith = 90.8333 -- Example zenith angle
-  local local_offset = 0 -- Example offset in minutes
-  local sunrise_time, sunset_time = lustrous.sunturn_time(date, true, lat, lng, zenith, local_offset),
-      lustrous.sunturn_time(date, false, lat, lng, zenith, local_offset)
-  switch_colorscheme(sunrise_time, sunset_time)
-end
-
-vim.schedule(check_time, 30000)
