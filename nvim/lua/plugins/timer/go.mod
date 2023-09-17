6b50637a7c1fca0e41f65abc3dd609088e0a72e4
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-17 16:40:30 +0530

    Fix plugins
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

---
+++ nvim/lua/plugins/timer/go.mod
@@ -0,0 +1,8 @@
+module github.com/gnanakeethan/dotfiles/nvim/lua/user/themes
+
+go 1.19
+
+require (
+	github.com/kelvins/sunrisesunset v0.0.0-20210220141756-39fa1bd816d5
+	github.com/nathan-osman/go-sunrise v1.1.0
+)
---
+++ nvim/lua/plugins/timer/timer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/nathan-osman/go-sunrise"
+)
+
+func main() {
+	// get latitude and longitiude from runtime arguments
+	lat, _ := strconv.ParseFloat(os.Args[1], 64)
+	lng, _ := strconv.ParseFloat(os.Args[2], 64)
+	sunrise, sunset := sunrise.SunriseSunset(
+		lat, lng, // Toronto, CA
+		time.Now().Year(), time.Now().Month(), time.Now().Day(),
+	)
+	if os.Args[3] == "sunrise" {
+		fmt.Println(sunrise.Unix())
+	} else {
+		fmt.Println(sunset.Unix())
+	}
+}
