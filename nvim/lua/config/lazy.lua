ee64ee7c6c7c145ccd2c8dbaa5206be66fd19a25
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2024-01-12 23:15:02 +0530

    configure copilot
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
     -- import any extras modules here
 
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    --{ import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,93 +1,146 @@
 return {
-  { "christoomey/vim-tmux-navigator" },
-  { "farmergreg/vim-lastplace" },
-  { "tpope/vim-commentary" },
-  { "tpope/vim-repeat" },
-  { "tpope/vim-surround" },
-  { "tpope/vim-eunuch" }, -- Adds :Rename, :SudoWrite,
-  { "tpope/vim-unimpaired" }, -- Adds [b and other handy mappings,
-  { "tpope/vim-sleuth" }, -- Indent autodetection with editorconfig support,
-  { "jessarcher/vim-heritage" }, -- Automatically create parent dirs when saving,
-  { "nelstrom/vim-visual-star-search" },
-  --{ "b0o/schemastore.nvim" },
-  { "nvim-tree/nvim-web-devicons" }, -- OPTIONAL: for file icons,
-  { "goolord/alpha-nvim", enabled = false },
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+    { "christoomey/vim-tmux-navigator" },
+    { "farmergreg/vim-lastplace" },
+    { "tpope/vim-commentary" },
+    { "tpope/vim-repeat" },
+    { "tpope/vim-surround" },
+    { "tpope/vim-eunuch" }, -- Adds :Rename, :SudoWrite,
+    { "tpope/vim-unimpaired" }, -- Adds [b and other handy mappings,
+    { "tpope/vim-sleuth" }, -- Indent autodetection with editorconfig support,
+    { "jessarcher/vim-heritage" }, -- Automatically create parent dirs when saving,
+    { "nelstrom/vim-visual-star-search" },
+    { "nvim-tree/nvim-web-devicons" }, -- OPTIONAL: for file icons,
+    { "goolord/alpha-nvim", enabled = false },
+    {
+        "telescope.nvim",
+        dependencies = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            config = function()
+                require("telescope").load_extension("fzf")
+            end,
+        },
     },
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("plugins.custom.nvim-tree")
-    end,
-  },
-  -- formatters
-  {
-    "nvimtools/none-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    config = function()
-      require("plugins.custom.null-ls")
-    end,
-  },
-  {
-    "sbdchd/neoformat",
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      {
-        "mason.nvim",
-        opts = function(_, opts)
-          opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "gofumpt", "goimports-reviser", "delve" })
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("plugins.custom.nvim-tree")
         end,
-      },
-      {
-        "leoluz/nvim-dap-go",
-        config = true,
-      },
     },
-  },
-  {
-    "lukas-reineke/headlines.nvim",
-    enabled = false,
-    opts = function()
-      local opts = {}
-      for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
-        opts[ft] = {
-          headline_highlights = {},
-        }
-        for i = 1, 6 do
-          local hl = "Headline" .. i
-          vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
-          table.insert(opts[ft].headline_highlights, hl)
-        end
-      end
-      return opts
-    end,
-    ft = { "markdown", "norg", "rmd", "org" },
-    config = function(_, opts)
-      -- PERF: schedule to prevent headlines slowing down opening a file
-      vim.schedule(function()
-        require("headlines").setup(opts)
-        require("headlines").refresh()
-      end)
-    end,
-  },
+    -- formatters
+    {
+        "nvimtools/none-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("plugins.custom.null-ls")
+        end,
+    },
+    {
+        "sbdchd/neoformat",
+    },
+    {
+        "mfussenegger/nvim-dap",
+        optional = true,
+        dependencies = {
+            {
+                "mason.nvim",
+                opts = function(_, opts)
+                    opts.ensure_installed = opts.ensure_installed or {}
+                    vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "gofumpt", "goimports-reviser", "delve" })
+                end,
+            },
+            {
+                "leoluz/nvim-dap-go",
+                config = true,
+            },
+        },
+    },
+    {
+        "lukas-reineke/headlines.nvim",
+        enabled = false,
+        opts = function()
+            local opts = {}
+            for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
+                opts[ft] = {
+                    headline_highlights = {},
+                }
+                for i = 1, 6 do
+                    local hl = "Headline" .. i
+                    vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+                    table.insert(opts[ft].headline_highlights, hl)
+                end
+            end
+            return opts
+        end,
+        ft = { "markdown", "norg", "rmd", "org" },
+        config = function(_, opts)
+            -- PERF: schedule to prevent headlines slowing down opening a file
+            vim.schedule(function()
+                require("headlines").setup(opts)
+                require("headlines").refresh()
+            end)
+        end,
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        build = ":Copilot auth",
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+            filetypes = {
+                markdown = true,
+                help = true,
+            },
+        },
+    },
+    {
+        "nvim-cmp",
+        dependencies = {
+            {
+                "zbirenbaum/copilot-cmp",
+                dependencies = "copilot.lua",
+                opts = {},
+                config = function(_, opts)
+                    local copilot_cmp = require("copilot_cmp")
+                    copilot_cmp.setup(opts)
+                    -- attach cmp source whenever copilot attaches
+                    -- fixes lazy-loading issues with the copilot cmp source
+                    require("lazyvim.util").lsp.on_attach(function(client)
+                        if client.name == "copilot" then
+                            copilot_cmp._on_insert_enter({})
+                        end
+                    end)
+                end,
+            },
+            'L3MON4D3/LuaSnip',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-nvim-lua',
+            'jessarcher/cmp-path',
+            'onsails/lspkind-nvim',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            table.insert(opts.sources, 1, {
+                name = "copilot",
+                group_index = 1,
+                priority = 100,
+            })
+        end,
+        config = function()
+            require('plugins.custom.cmp')
+        end,
+    }
 }
 --use({
 --    'tommcdo/vim-lion',
