d7d8104b8bd5c5eb7af1ba02564f83980d24424f
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2024-01-13 18:56:01 +0530

    nvim-tree float enabled
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- nvim/lua/plugins/custom/nvim-tree.lua
+++ nvim/lua/plugins/custom/nvim-tree.lua
@@ -1,13 +1,39 @@
 -- vim.g.nvim_tree_indent_markers = 1
 -- vim.g.nvim_tree_git_hl = 1
+local HEIGHT_RATIO = 0.9 -- You can change this
+local WIDTH_RATIO = 0.3 -- You can change this too
 
 require("nvim-tree").setup({
   git = {
     ignore = true,
   },
   view = {
-    side = "left",
-    width = 30,
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = -2,
+          col = -2,
+
+          width = window_w_int,
+          height = window_h_int,
+        }
+      end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+    -- width = 30,
   },
   renderer = {
     highlight_opened_files = "all",
