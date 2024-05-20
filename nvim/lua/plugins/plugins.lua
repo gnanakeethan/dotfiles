839552581058fd64c8b520d4b1a1c8b59ab4c1ae
Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>
2024-05-20 12:03:46 +0530

    fixes
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,7 +1,6 @@
 return {
   { "christoomey/vim-tmux-navigator" },
   { "farmergreg/vim-lastplace" },
-  { "tpope/vim-commentary" },
   { "tpope/vim-repeat" },
   { "tpope/vim-surround" },
   { "tpope/vim-eunuch" }, -- Adds :Rename, :SudoWrite,
@@ -209,6 +208,15 @@ return {
   --  },
   --},
   { "nvim-neotest/nvim-nio" },
+  {
+    "glacambre/firenvim",
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+      vim.fn["firenvim#install"](0)
+    end,
+  },
   {
     "leoluz/nvim-dap-go",
     dependencies = {
@@ -436,6 +444,7 @@ return {
   },
   {
     "nvim-cmp",
+    event = { "LazyFile", "VeryLazy" },
     dependencies = {
       {
         "zbirenbaum/copilot-cmp",
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,19 @@
-
 return {
-    {
-        'EdenEast/nightfox.nvim',
-        config = function()
-            local latitude = 6.99
-            local longitude = 79.99
-            local sunrise = io.popen("timer " .. latitude .. " " .. longitude .. " sunrise"):read("*all")
-            local sunset = io.popen("timer " .. latitude .. " " .. longitude .. " sunset"):read("*all")
-            local now = os.time()
+  {
+    "EdenEast/nightfox.nvim",
+    event = { "LazyFile", "VeryLazy" },
+    config = function()
+      local latitude = 6.99
+      local longitude = 79.99
+      local sunrise = io.popen("timer " .. latitude .. " " .. longitude .. " sunrise"):read("*all")
+      local sunset = io.popen("timer " .. latitude .. " " .. longitude .. " sunset"):read("*all")
+      local now = os.time()
 
-            if now > tonumber(sunrise) and now < tonumber(sunset) then
-                vim.api.nvim_command("colorscheme dayfox")
-            else
-                vim.api.nvim_command("colorscheme nightfox")
-            end
-        end,
-    }
+      if now > tonumber(sunrise) and now < tonumber(sunset) then
+        vim.api.nvim_command("colorscheme dayfox")
+      else
+        vim.api.nvim_command("colorscheme nightfox")
+      end
+    end,
+  },
 }
