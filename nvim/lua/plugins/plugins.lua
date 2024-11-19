51b65ade672bda696b1271a143d2a03623e563bd
Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>
2024-11-19 18:21:16 +0530

    update: dashboard updated snacks.nvim
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -317,6 +317,60 @@ return {
       end)
     end,
   },
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      bigfile = { enabled = true },
+      dashboard = {
+        preset = {
+          header = [[ 
+ ▗▄▄▗▖   ▗▄▖▗▖ ▗▗▄▄▄     ▗▄▄▖ ▗▄▖▗▄▄▖ ▗▄▖▗▖  ▗▖   ▗▄▖▗▖  ▗▖
+▐▌  ▐▌  ▐▌ ▐▐▌ ▐▐▌  █    ▐▌ ▐▐▌ ▐▐▌ ▐▐▌ ▐▐▌  ▐▌  ▐▌ ▐▌▝▚▞▘ 
+▐▌  ▐▌  ▐▌ ▐▐▌ ▐▐▌  █    ▐▛▀▘▐▛▀▜▐▛▀▚▐▛▀▜▐▌  ▐▌  ▐▛▀▜▌ ▐▌  
+▝▚▄▄▐▙▄▄▝▚▄▞▝▚▄▞▐▙▄▄▀    ▐▌  ▐▌ ▐▐▌ ▐▐▌ ▐▐▙▄▄▐▙▄▄▐▌ ▐▗▞▘▝▚▖
+
+ 1111B S Governors Ave STE 21786, Dover, DE 19904
+                ]],
+        },
+        enabled = true,
+        sections = {
+          { section = "header", panes = 2 },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 5 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = vim.fn.isdirectory(".git") == 1,
+            cmd = "hub status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
+      notifier = {
+        enabled = true,
+        timeout = 3000,
+      },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      styles = {
+        notification = {
+          wo = { wrap = true }, -- Wrap notifications
+        },
+      },
+    },
+  },
   {
     "nvim-cmp",
     event = { "LazyFile", "VeryLazy" },
