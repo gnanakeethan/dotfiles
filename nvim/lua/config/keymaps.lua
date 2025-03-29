c79d9536ca2e4378ecd5a98d1e885dc7fcea3908
Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>
2025-03-30 05:15:46 +0530

    fix: enable certain keymaps
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@cloudparallax.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,13 +10,13 @@
 -- vim.keymap.set("", "gf", ":edit <cfile><CR>")
 --
 -- -- Reselect visual selection after indenting
--- vim.keymap.set("v", "<", "<gv")
--- vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 --
 -- -- Maintain the cursor position when yanking a visual selection
 -- -- http://ddrscott.github.io/blog/2016/yank-without-jank/
--- vim.keymap.set("v", "y", "myy`y")
--- vim.keymap.set("v", "Y", "myY`y")
+vim.keymap.set("v", "y", "myy`y")
+vim.keymap.set("v", "Y", "myY`y")
 --
 -- -- When text is wrapped, move by terminal rows, not lines, unless a count is provided
 -- vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
@@ -30,7 +30,7 @@
 -- vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
 --
 -- -- Open the current file in the default program (on Mac this should just be just `open`)
--- vim.keymap.set("n", "<leader>x", ":!open %<cr><cr>")
+vim.keymap.set("n", "<leader>x", ":!open %<cr><cr>")
 --
 -- -- Disable annoying command line thing
 -- vim.keymap.set("n", "q:", ":q<CR>")
